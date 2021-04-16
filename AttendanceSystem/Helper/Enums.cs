69d030eb3175e2f3bb6becaaa847a23e360b4505
Nilesh Prajapati <nilesh.prajapati.hs@gmail.com>
2021-04-16 22:47:25 +0530

    helper

---
+++ AttendanceSystem/Helper/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AttendanceSystem.Helper
+{
+
+    public enum AdminRoles
+    {
+        SuperAdmin = 1,
+        CompanyAdmin = 2,
+        Employee = 3,
+        Supervisor = 4,
+        Checker = 5,
+        Payer = 6,
+        Worker = 7
+    }
+
+}
---
+++ AttendanceSystem/Helper/ErrorMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMobileApp.Helper
+{
+    public static class ErrorMessage
+    {
+        // Admin Portal Messages
+        public static string GstNumberExists = "GST Number is already exist";
+
+        // Folder Directory Path
+        public static string UserDirectoryPath = "/Images/UserMedia/";
+
+        // Default Images
+        public static string DefaultImagePath = "/Images/default_image.png";
+        public static string DefaultUserImagePath = "/Images/default_user_image.png";
+
+    }
+}
