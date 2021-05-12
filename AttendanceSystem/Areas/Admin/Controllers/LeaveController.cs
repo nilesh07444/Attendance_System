fbd5e37fac73a568947ca347494bb563739055de
cserpsolutions <cserpsolutions@gmail.com>
2021-05-12 08:48:10 +0530

    leave approve reject and cancelled

--- AttendanceSystem/Areas/Admin/Controllers/LeaveController.cs
+++ AttendanceSystem/Areas/Admin/Controllers/LeaveController.cs
@@ -4,7 +4,6 @@ using AttendanceSystem.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AttendanceSystem.Areas.Admin.Controllers
@@ -13,9 +12,13 @@ namespace AttendanceSystem.Areas.Admin.Controllers
     {
         // GET: Admin/Leave
         AttendanceSystemEntities _db;
+        long companyId;
+        long LoggedInUserId;
         public LeaveController()
         {
             _db = new AttendanceSystemEntities();
+            companyId = clsAdminSession.CompanyId;
+            LoggedInUserId = clsAdminSession.UserID;
         }
         public ActionResult Index(int? userRole = null, int? leaveStatus = null, DateTime? startDate = null, DateTime? endDate = null)
         {
@@ -33,16 +36,16 @@ namespace AttendanceSystem.Areas.Admin.Controllers
                     leaveFIlterVM.EndDate = endDate.Value;
                 }
 
-                long companyId = clsAdminSession.CompanyId;
+
 
                 leaveFIlterVM.LeaveList = (from lv in _db.tbl_Leave
-                                           join ur in _db.tbl_AdminUser on lv.UserId equals ur.AdminUserId
+                                           join ur in _db.tbl_Employee on lv.UserId equals ur.EmployeeId
                                            join cm in _db.tbl_Company on ur.CompanyId equals cm.CompanyId
                                            where !lv.IsDeleted
                                            && cm.CompanyId == companyId
                                            && lv.StartDate >= leaveFIlterVM.StartDate && lv.StartDate <= leaveFIlterVM.EndDate
                                            && (leaveFIlterVM.LeaveStatus.HasValue ? lv.LeaveStatus == leaveFIlterVM.LeaveStatus.Value : true)
-                                           && (leaveFIlterVM.UserRole.HasValue ? ur.AdminUserRoleId == leaveFIlterVM.LeaveStatus.Value : true)
+                                           && (leaveFIlterVM.UserRole.HasValue ? ur.AdminRoleId == leaveFIlterVM.UserRole.Value : true)
 
                                            select new LeaveVM
                                            {
@@ -56,6 +59,11 @@ namespace AttendanceSystem.Areas.Admin.Controllers
                                                RejectReason = lv.RejectReason,
                                                CancelledReason = lv.CancelledReason
                                            }).OrderByDescending(x => x.StartDate).ToList();
+
+                leaveFIlterVM.LeaveList.ForEach(x =>
+                {
+                    x.LeaveStatusText = CommonMethod.GetEnumDescription((LeaveStatus)x.LeaveStatus);
+                });
                 leaveFIlterVM.UserRoleList = GetUserRoleList();
             }
             catch (Exception ex)
@@ -75,6 +83,104 @@ namespace AttendanceSystem.Areas.Admin.Controllers
                                             Value = ms.AdminRoleId.ToString()
                                         }).ToList();
             return lst;
+        }
+
+        [HttpPost]
+        public string AcceptLeave(string ids)
+        {
+            string ReturnMessage = "";
+            try
+            {
+                long loggedinUser = clsAdminSession.UserID;
+                List<tbl_Leave> leaveList = _db.tbl_Leave.Where(x => ids.Contains(x.LeaveId.ToString())).ToList();
+
+                if (leaveList != null)
+                {
+                    leaveList.ForEach(leave =>
+                    {
+                        leave.LeaveStatus = (int)LeaveStatus.Accept;
+                        leave.ModifiedBy = loggedinUser;
+                        leave.ModifiedDate = DateTime.UtcNow;
+                    });
+
+                    _db.SaveChanges();
+                    ReturnMessage = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                ReturnMessage = "exception";
+            }
+
+            return ReturnMessage;
+        }
+
+        public ActionResult Reject(long id)
+        {
+            LeaveVM leaveVM = new LeaveVM();
+            try
+            {
+                leaveVM = (from lv in _db.tbl_Leave
+                           join ur in _db.tbl_Employee on lv.UserId equals ur.EmployeeId
+                           join cm in _db.tbl_Company on ur.CompanyId equals cm.CompanyId
+                           where !lv.IsDeleted
+                           && cm.CompanyId == companyId
+                           && lv.LeaveId == id
+                           select new LeaveVM
+                           {
+                               LeaveId = lv.LeaveId,
+                               UserId = lv.UserId,
+                               UserName = ur.FirstName,
+                               StartDate = lv.StartDate,
+                               EndDate = lv.EndDate,
+                               NoOfDays = lv.NoOfDays,
+                               LeaveStatus = lv.LeaveStatus,
+                               RejectReason = lv.RejectReason,
+                               CancelledReason = lv.CancelledReason
+                           }).FirstOrDefault();
+
+                leaveVM.LeaveStatusText = CommonMethod.GetEnumDescription((LeaveStatus)leaveVM.LeaveStatus);
+            }
+            catch (Exception ex)
+            {
+                string ErrorMessage = ex.Message.ToString();
+                throw ex;
+            }
+            return View(leaveVM);
+        }
+
+        [HttpPost]
+        public ActionResult Reject(LeaveVM leaveVM)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    tbl_Leave objleave = _db.tbl_Leave.FirstOrDefault(x => x.LeaveId == leaveVM.LeaveId);
+                    if (objleave != null)
+                    {
+                        objleave.LeaveStatus = leaveVM.LeaveStatus;
+                        objleave.CancelledReason = leaveVM.CancelledReason;
+                        objleave.RejectReason = leaveVM.RejectReason;
+                        objleave.ModifiedBy = LoggedInUserId;
+                        objleave.ModifiedDate = DateTime.UtcNow;
+                        _db.SaveChanges();
+                    }
+                     
+                }
+                else
+                {
+                    return View(leaveVM);
+                }
+            }
+            catch (Exception ex)
+            {
+                string ErrorMessage = ex.Message.ToString();
+                throw ex;
+            }
+            return RedirectToAction("index");
         }
     }
 }
--- AttendanceSystem/ViewModel/LeaveVM.cs
+++ AttendanceSystem/ViewModel/LeaveVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,20 @@ namespace AttendanceSystem.ViewModel
     {
         public long LeaveId { get; set; }
         public long UserId { get; set; }
+        [Display(Name = "Employee Name")]
         public string UserName { get; set; }
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+        [Display(Name = "No Of Days")]
         public decimal NoOfDays { get; set; }
+        [Display(Name = "Leave Status")]
         public int LeaveStatus { get; set; }
+        public string LeaveStatusText { get; set; }
+        [Display(Name = "Reject Reason")]
         public string RejectReason { get; set; }
+        [Display(Name = "Cancelled Reason")]
         public string CancelledReason { get; set; }
         public bool IsDeleted { get; set; }
 
