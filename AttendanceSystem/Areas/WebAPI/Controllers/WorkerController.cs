62e9d17484fd942599467c4e9256edeb582ab680
Nilesh Prajapati <nilesh.prajapati.hs@gmail.com>
2021-06-03 17:48:53 +0530

    added search worker API

--- AttendanceSystem/Areas/WebAPI/Controllers/WorkerController.cs
+++ AttendanceSystem/Areas/WebAPI/Controllers/WorkerController.cs
@@ -1,5 +1,6 @@
 ﻿using AttendanceSystem.Helper;
 using AttendanceSystem.Models;
+using AttendanceSystem.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -14,12 +15,16 @@ namespace AttendanceSystem.Areas.WebAPI.Controllers
     public class WorkerController : BaseUserController
     {
         private readonly AttendanceSystemEntities _db;
-
+        long employeeId;
+        long companyId;
+        int roleId;
+        string domainUrl = string.Empty;
         public WorkerController()
         {
             _db = new AttendanceSystemEntities();
+            domainUrl = ConfigurationManager.AppSettings["DomainUrl"].ToString();
         }
-         
+
         [HttpPost]
         [Route("AddFingerprint")]
         public ResponseDataModel<bool> AddFingerprint(EmployeeFirgerprintVM fingerprintVM)
@@ -68,6 +73,56 @@ namespace AttendanceSystem.Areas.WebAPI.Controllers
             return response;
         }
 
+
+        [HttpGet]
+        [Route("Search")]
+        public ResponseDataModel<List<EmployeeVM>> Search(string searchText)
+        {
+            ResponseDataModel<List<EmployeeVM>> response = new ResponseDataModel<List<EmployeeVM>>();
+            response.IsError = false;
+            try
+            {
+                companyId = base.UTI.CompanyId;
+
+                List<EmployeeVM> workerList = (from emp in _db.tbl_Employee
+                                               where !emp.IsDeleted && emp.IsActive
+                                               && emp.WorkerTypeId == (int)AdminRoles.Worker
+                                               && emp.CompanyId == companyId
+                                               && (!string.IsNullOrEmpty(searchText) ? (emp.EmployeeCode.Contains(searchText)
+                                               || emp.FirstName.Contains(searchText)
+                                               || emp.LastName.Contains(searchText)) : true)
+                                               select new EmployeeVM
+                                               {
+                                                   EmployeeId = emp.EmployeeId,
+                                                   EmployeeCode = emp.EmployeeCode,
+                                                   CompanyId = emp.CompanyId,
+                                                   Prefix = emp.Prefix,
+                                                   FirstName = emp.FirstName,
+                                                   LastName = emp.LastName,
+                                                   MobileNo = emp.MobileNo,
+                                                   Dob = emp.Dob,
+                                                   DateOfJoin = emp.DateOfJoin,
+                                                   BloodGroup = emp.BloodGroup,
+                                                   EmploymentCategory = emp.EmploymentCategory,
+                                                   MonthlySalaryPrice = emp.MonthlySalaryPrice,
+                                                   AdharCardNo = emp.AdharCardNo,
+                                                   Address = emp.Address,
+                                                   City = emp.City,
+                                                   Pincode = emp.Pincode,
+                                                   State = emp.State,
+                                                   IsActive = emp.IsActive,
+                                                   ProfilePicture = !string.IsNullOrEmpty(emp.ProfilePicture) ? domainUrl + ErrorMessage.EmployeeDirectoryPath + emp.ProfilePicture : string.Empty
+                                               }).ToList();
+                response.Data = workerList;
+            }
+            catch (Exception ex)
+            {
+                response.IsError = true;
+                response.AddError(ex.Message);
+            }
+
+            return response;
+        }
 
     }
 }
