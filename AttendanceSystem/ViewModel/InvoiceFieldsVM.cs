80a96068fe9f1adaad56f808329dcc3a4dfaa479
cserpsolutions <cserpsolutions@gmail.com>
2021-08-01 18:23:00 +0530

    invoive copy

---
+++ AttendanceSystem/ViewModel/InvoiceFieldsVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AttendanceSystem.ViewModel
+{
+    public class InvoiceFieldsVM
+    {
+        public string CompanyName { get; set; }
+        public string InvoiceNo { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime InvoiceDatetime { get; set; }
+        public string InvoiceDate { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string GstNo { get; set; }
+        public string PanNo { get; set; }
+        public string PackageName { get; set; }
+        public string HsnCode { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Rate { get; set; }
+        public decimal GSTRate { get; set; }
+        public decimal CGST { get; set; }
+        public decimal SGST { get; set; }
+        public decimal IGST { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
